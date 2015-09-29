373498e45fa448983ce8cbbf4d31d1c60dfab900
nicolas silva <sovietfrance@gmail.com>
2015-09-28 23:20:45 -0300

    add first model for senate and senators

--- chileanApi/congress/models.py
+++ chileanApi/congress/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Senate(models.Model):
+  name = models.CharField(max_length=255)
+
+class Senator(models.Model):
+  senate = models.ForeignKey(Senate)
+  name = models.CharField(max_length=255)
+  last_name = models.CharField(max_length=255)
+  years = models.CharField(max_length=255)
+  phone = models.CharField(max_length=255)
