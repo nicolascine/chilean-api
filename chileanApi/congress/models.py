844719b6ae1fe3ae4341bdb514f7cd72ff6817ae
nicolas silva <sovietfrance@gmail.com>
2015-09-30 23:12:35 -0300

    add Party Model

--- chileanApi/congress/models.py
+++ chileanApi/congress/models.py
@@ -3,8 +3,12 @@ from django.db import models
 class Senate(models.Model):
   name = models.CharField(max_length=255)
 
+class Party(model.Model):
+  name = models.CharField(max_length=255)
+
 class Senator(models.Model):
   senate = models.ForeignKey(Senate)
+  party = models.ForeignKey(Senate)
   name = models.CharField(max_length=255)
   last_name = models.CharField(max_length=255)
   years = models.CharField(max_length=255)
